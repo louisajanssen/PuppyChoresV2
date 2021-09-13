e6a98c772a80130290d41e39e6cfb27ea8ca0cd6
louisajanssen <janssenlouisa1@gmail.com>
2021-09-13 14:23:19 -0700

    starting on cards

--- client/Activity.tsx
+++ client/Activity.tsx
@@ -1,20 +1,92 @@
 import * as React from "react";
 import { IfoodActivity, IpottyActivity } from './Datatypes';
-import { Stack, IStackTokens } from '@fluentui/react/lib/Stack';
+import { Text, DefaultPalette, Stack, IStackStyles, IStackTokens, IStackItemStyles, getTheme } from '@fluentui/react';
 
 interface IProps {
   moment: IpottyActivity | IfoodActivity
 }
 
+const stackStyles: IStackStyles = {
+  root: {
+    background: DefaultPalette.white,
+  },
+};
+
+const stackItemStyles: IStackItemStyles = {
+  root: {
+    alignItems: 'center',
+    background: DefaultPalette.white,
+    color: DefaultPalette.black,
+    display: 'flex',
+    height: 50,
+    justifyContent: 'center',
+  },
+};
+
+const mainStackItemStyles: IStackItemStyles ={
+  root: {
+      background: DefaultPalette.white,
+      color: DefaultPalette.black,
+      padding: 5,
+    },
+}
+
+const divStyles = {
+  padding: 10
+}
+
+const theme = getTheme();
+
+const mainDivStyles = {
+  boxShadow: theme.effects.elevation64,
+  width: 600,
+  padding: 25,
+  align: 'center'
+}
+
+const stackTokens: IStackTokens = {
+  childrenGap: 5,
+  padding: 10,
+};
+
+const itemAlignmentsStackTokens: IStackTokens = {
+  childrenGap: 5,
+  padding: 10,
+};
+
 const containerStackTokens: IStackTokens = { childrenGap: 5 };
 
 export const Activity: React.FC<IProps>= ({moment}: IProps) => {
 
-  
+
   if (moment.activityType === 'potty') {
+    const getPottyType = () => {
+      if (moment.pottyType === "pee") {
+        return "I went Number 1!"
+      } else {
+        return "I went Number 2!"
+      }
+    }
     return (
-      <Stack tokens={containerStackTokens}>
-        <Stack.Item align="center">Potty: {moment.pottyType}</Stack.Item>
+      <Stack styles={stackStyles} tokens={itemAlignmentsStackTokens}>
+        <Stack.Item align="center" styles={mainStackItemStyles}>
+          <div style={mainDivStyles}>
+          <Stack horizontal styles={stackStyles} tokens={stackTokens}>
+            <Stack.Item grow={2} styles={stackItemStyles}>
+              <Text>{getPottyType()}</Text>
+            </Stack.Item>
+            <Stack.Item grow={2} styles={stackItemStyles}>
+              <Text variant="medium">on</Text>
+            </Stack.Item>
+            <Stack.Item grow={2} styles={stackItemStyles}>
+              Date
+            </Stack.Item>
+            <div style={divStyles}>
+              <Text>{moment.notes}</Text>
+            </div>
+          </Stack>
+         </div>
+        </Stack.Item>
       </Stack>
     )
   }
